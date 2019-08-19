9fc303933782766fd5c20e73bff40173d48579bb
Jeong Park <wjdtn1220@gmail.com>
2019-08-19 00:47:22 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,8 +16,7 @@ def remove_strawberry(contacts)
   contacts.each do |person, data|
     data.each do |attribute, value|
       if attribute = :favorite_ice_cream_flavors
-        value.each do |flavor|
-          puts favorite_ice_cream_flavors.shift("strawberry")
+        favorite_ice_cream_flavors.shift("strawberry")
         end
       end
     end
